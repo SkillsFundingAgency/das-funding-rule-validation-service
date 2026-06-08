77c23fc4985d9370d6feee1df95cffcd27056ca5
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-06-08 16:47:52 +0100

    FAI-3443 - Add function endpoint

---
+++ src/SFA.DAS.FundingRuleValidation/Endpoints/DurableTrigger.cs
@@ -0,0 +1,21 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.DurableTask.Client;
+
+namespace SFA.DAS.FundingRuleValidation.Jobs.Endpoints;
+
+public class FundingRuleHttpEndpoint
+{
+    [Function("DurableFunctionsOrchestrationCSharp1_HttpStart")]
+    public static async Task<HttpResponseData> HttpStart(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        [DurableClient] DurableTaskClient client,
+        FunctionContext executionContext)
+    {
+        var instanceId = 0;
+        var response = req.CreateResponse(HttpStatusCode.Accepted);
+        return response;
+    }    
+}
+
