d13e28099d60ad3b4a62dae2ad5cd9a306cf1d60
killij <51908793+killij@users.noreply.github.com>
2026-06-09 13:33:15 +0100

    FAI-3443 Add OpenTelemetry and initialisation from AzureTableStorage

---
+++ src/SFA.DAS.FundingRuleValidation.Jobs/Core/Configuration/HostBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
+using SFA.DAS.Configuration.AzureTableStorage;
+
+namespace SFA.DAS.FundingRuleValidation.Jobs.Core.Configuration;
+
+[ExcludeFromCodeCoverage]
+public static class HostBuilderExtensions
+{
+    public static FunctionsApplicationBuilder ConfigureFundingRuleValidationApp(this FunctionsApplicationBuilder builder)
+    {
+        builder
+            .ConfigureFunctionsWebApplication();
+            
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddEnvironmentVariables()
+            .AddJsonFile("local.settings.json", optional: true);
+            
+        var environmentName = builder.Configuration.GetValue<string>("Values:EnvironmentName") ?? builder.Configuration.GetValue<string>("EnvironmentName");
+        if (!environmentName!.Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
+        {
+            builder.Configuration.AddAzureTableStorage(options =>
+            {
+                options.ConfigurationNameIncludesVersionNumber = true;
+                options.PreFixConfigurationKeys = false;
+#if DEBUG
+                options.ConfigurationKeys = builder.Configuration.GetValue<string>("Values:ConfigNames")!.Split(",");
+                options.StorageConnectionString = builder.Configuration.GetValue<string>("Values:ConfigurationStorageConnectionString");
+                options.EnvironmentName = builder.Configuration.GetValue<string>("Values:EnvironmentName");
+#else
+                    options.ConfigurationKeys = builder.Configuration.GetValue<string>("ConfigNames")!.Split(",");
+                    options.StorageConnectionString = builder.Configuration.GetValue<string>("ConfigurationStorageConnectionString");
+                    options.EnvironmentName = builder.Configuration.GetValue<string>("EnvironmentName");
+#endif
+            });
+        }
+        
+        builder.Services.AddOpenTelemetryRegistration(builder.Configuration.GetValue<string>("APPLICATIONINSIGHTS_CONNECTION_STRING"));
+        return builder;
+    }
+}
---
+++ src/SFA.DAS.FundingRuleValidation.Jobs/Core/Configuration/OpenTelemetryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Monitor.OpenTelemetry.Exporter;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SFA.DAS.FundingRuleValidation.Jobs.Core.Configuration;
+
+public static class OpenTelemetryExtensions
+{
+    public static void AddOpenTelemetryRegistration(this IServiceCollection services, string? connectionString)
+    {
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            return;
+        }
+        
+        services.AddOpenTelemetry().UseAzureMonitorExporter(options => { options.ConnectionString = connectionString; });
+    }
+}
