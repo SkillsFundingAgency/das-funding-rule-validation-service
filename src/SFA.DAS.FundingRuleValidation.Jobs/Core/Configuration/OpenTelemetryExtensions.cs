b35a0339aa50bee33ec2f63a287b31adc7648e3b
killij <51908793+killij@users.noreply.github.com>
2026-06-10 11:29:27 +0100

    FAI-3443 Add Unit testing project

--- src/SFA.DAS.FundingRuleValidation.Jobs/Core/Configuration/OpenTelemetryExtensions.cs
+++ src/SFA.DAS.FundingRuleValidation.Jobs/Core/Configuration/OpenTelemetryExtensions.cs
@@ -1,8 +1,10 @@
-﻿using Azure.Monitor.OpenTelemetry.Exporter;
+﻿using System.Diagnostics.CodeAnalysis;
+using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace SFA.DAS.FundingRuleValidation.Jobs.Core.Configuration;
 
+[ExcludeFromCodeCoverage]
 public static class OpenTelemetryExtensions
 {
     public static void AddOpenTelemetryRegistration(this IServiceCollection services, string? connectionString)
