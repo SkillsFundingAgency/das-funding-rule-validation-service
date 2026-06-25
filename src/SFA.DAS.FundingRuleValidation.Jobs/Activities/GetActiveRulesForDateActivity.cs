582698f4e4f7b19266e014f57e69d83e614ba774
killij <51908793+killij@users.noreply.github.com>
2026-06-25 09:20:31 +0100

    FAI-3482 Add logging of matched rules when fetching

--- src/SFA.DAS.FundingRuleValidation.Jobs/Activities/GetActiveRulesForDateActivity.cs
+++ src/SFA.DAS.FundingRuleValidation.Jobs/Activities/GetActiveRulesForDateActivity.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.FundingRuleValidation.Jobs.Data;
 using SFA.DAS.FundingRuleValidation.Jobs.Domain;
 
 namespace SFA.DAS.FundingRuleValidation.Jobs.Activities;
 
-public class GetActiveRulesForDateActivity(IRulesRepository rulesRepository)
+public class GetActiveRulesForDateActivity(IRulesRepository rulesRepository, ILogger<GetActiveRulesForDateActivity> logger)
 {
     [Function(nameof(GetActiveRulesForDate))]
     public async Task<List<FundingRule>> GetActiveRulesForDate([ActivityTrigger] DateTime date, FunctionContext executionContext)
     {
-        return await rulesRepository.GetActiveRulesForDate(date);
-    }    
+        var result = await rulesRepository.GetActiveRulesForDate(date);
+        if (result is not { Count: > 0 })
+        {
+            logger.LogInformation("Returned no rules for {QueryDate:o}", date);
+            return [];
+        }
+
+        var ruleNames = string.Join(", ", result.Select(x => x.RuleName));
+        logger.LogInformation("Returned the following rules for {QueryDate:o} '{RuleNames}'", date, ruleNames);
+        return result;
+    }
 }
