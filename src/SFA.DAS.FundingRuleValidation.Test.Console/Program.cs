48138885d472b8804d1056e14859c1b7c3dd2864
killij <51908793+killij@users.noreply.github.com>
2026-06-25 09:24:11 +0100

    FAI-3482 Update test console to show returned message

--- src/SFA.DAS.FundingRuleValidation.Test.Console/Program.cs
+++ src/SFA.DAS.FundingRuleValidation.Test.Console/Program.cs
@@ -8,22 +8,47 @@ using SFA.DAS.FundingRuleValidation.Jobs.Domain;
 var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
 var adminConnectionString = config["AdminServiceBusConnection"];
 var connectionString = config[GlobalConstants.ServiceBusConnectionName];
-const string queueName = "validate-learner";
 
 // create the queue if it doesn't exist
 var adminClient = new ServiceBusAdministrationClient(adminConnectionString);
 if (await adminClient.QueueExistsAsync(GlobalConstants.IncomingQueueName) == false)
 {
-    await adminClient.CreateQueueAsync(new CreateQueueOptions(queueName));
+    await adminClient.CreateQueueAsync(new CreateQueueOptions(GlobalConstants.IncomingQueueName));
+}
+
+if (await adminClient.QueueExistsAsync(GlobalConstants.OutgoingQueueName) == false)
+{
+    await adminClient.CreateQueueAsync(new CreateQueueOptions(GlobalConstants.OutgoingQueueName));
 }
 
 // send a message
 await using var client = new ServiceBusClient(connectionString);
-await using var sender = client.CreateSender(queueName);
+await using var sender = client.CreateSender(GlobalConstants.IncomingQueueName);
+await using var receiver = client.CreateReceiver(GlobalConstants.OutgoingQueueName);
 
 const long ukprn = 10000;
 const long uln = 1234567890;
-var courses = new List<Course>();
+var courses = new List<Course>
+{
+    new()
+    {
+        Id = "Course1",
+        AgeAtStartOfCourse = 27,
+        StartDate = DateTime.Now.AddMonths(6),
+        EndDate = DateTime.UtcNow.AddMonths(12),
+        PlannedEndDate = DateTime.UtcNow.AddMonths(12),
+        Status = LearnerCourseStatus.InLearning,
+        TrainingType = TrainingType.Apprenticeship,
+        Type = CourseType.Apprenticeship,
+    }
+};
 
 var command = new ValidateLearnerCommand(Guid.NewGuid(), ukprn, uln, courses);
 await sender.SendMessageAsync(new ServiceBusMessage(JsonSerializer.Serialize(command)));
+
+Console.WriteLine("Waiting for result");
+var message = await receiver.ReceiveMessageAsync(TimeSpan.FromSeconds(300));
+await receiver.CompleteMessageAsync(message);
+
+Console.WriteLine($"Received message: {message.Body}");
+Console.ReadLine();
