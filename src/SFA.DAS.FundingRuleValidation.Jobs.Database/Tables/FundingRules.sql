05e9208421237179a69cb24c6877870b5020ebfd
killij <51908793+killij@users.noreply.github.com>
2026-07-20 13:38:14 +0100

    FAI-3506 Update the SQL script for new IlrRuleDescription column

--- src/SFA.DAS.FundingRuleValidation.Jobs.Database/Tables/FundingRules.sql
+++ src/SFA.DAS.FundingRuleValidation.Jobs.Database/Tables/FundingRules.sql
@@ -2,6 +2,7 @@ CREATE TABLE dbo.[FundingRules] (
     [Id]                            UNIQUEIDENTIFIER        NOT NULL,
     [RuleName]                      NVARCHAR(255)           NOT NULL,
     [IlrRuleName]                   NVARCHAR(255)           NOT NULL,
+    [IlrRuleDescription]            NVARCHAR(500)           NOT NULL,
     [Enabled]                       BIT                     NOT NULL DEFAULT 1,
     [EffectiveFrom]                 DATETIMEOFFSET          NOT NULL,
     [EffectiveTo]                   DATETIMEOFFSET          NOT NULL,
