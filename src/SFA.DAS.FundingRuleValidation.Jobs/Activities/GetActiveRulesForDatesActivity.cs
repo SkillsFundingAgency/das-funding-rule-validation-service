f6079e15c353f12b57042dca3d94b40f623638a0
killij <51908793+killij@users.noreply.github.com>
2026-07-27 15:23:11 +0100

    FAI-3584 Use log scopes and make some logging debug rather than info

--- src/SFA.DAS.FundingRuleValidation.Jobs/Activities/GetActiveRulesForDatesActivity.cs
+++ src/SFA.DAS.FundingRuleValidation.Jobs/Activities/GetActiveRulesForDatesActivity.cs
@@ -5,23 +5,25 @@ using SFA.DAS.FundingRuleValidation.Jobs.Domain;
 
 namespace SFA.DAS.FundingRuleValidation.Jobs.Activities;
 
-public class GetActiveRulesForDatesActivity(IRulesRepository rulesRepository, ILogger<GetActiveRulesForDatesActivity> logger)
+public partial class GetActiveRulesForDatesActivity(IRulesRepository rulesRepository, ILogger<GetActiveRulesForDatesActivity> logger)
 {
     [Function(nameof(GetActiveRulesForDatesActivity))]
     public async Task<List<FundingRule>> Run([ActivityTrigger] List<DateTime> dates, FunctionContext executionContext)
     {
         var result = await rulesRepository.GetActiveRulesForDatesAsync(dates, executionContext.CancellationToken);
-        
         if (result is not { Count: > 0 })
         {
-            logger.LogInformation("Returned no rules for dates: {QueryDate:o}", string.Join(", ", dates));
+            LogNoRules(string.Join(", ", dates));
             return [];
         }
 
-        logger.LogInformation(
-            "Returned the following rules for dates: {QueryDate:o} '{RuleNames}'",
-            string.Join(", ", dates),
-            string.Join(", ", result.Select(x => x.RuleName)));
+        LogFoundRules(string.Join(", ", dates), string.Join(", ", result.Select(x => x.RuleName)));
         return result;
     }
+
+    [LoggerMessage(LogLevel.Debug, "Returned the following rules for dates: {QueryDate:o} '{RuleNames}'")]
+    partial void LogFoundRules(string queryDate, string ruleNames);
+
+    [LoggerMessage(LogLevel.Debug, "Returned no rules for dates: {QueryDate:o}")]
+    partial void LogNoRules(string queryDate);
 }
--- src/SFA.DAS.FundingRuleValidation.Jobs/Activities/SendValidationResultActivity.cs
+++ src/SFA.DAS.FundingRuleValidation.Jobs/Activities/SendValidationResultActivity.cs
@@ -7,7 +7,7 @@ using SFA.DAS.FundingRuleValidation.Jobs.Domain;
 
 namespace SFA.DAS.FundingRuleValidation.Jobs.Activities;
 
-public class SendValidationResultActivity(ServiceBusClient serviceBusClient, ILogger<SendValidationResultActivity> logger)
+public partial class SendValidationResultActivity(ServiceBusClient serviceBusClient, ILogger<SendValidationResultActivity> logger)
 {
     [Function(nameof(SendValidationResultActivity))]
     public async Task Run([ActivityTrigger] ValidateLearnerResult result, FunctionContext context)
@@ -15,6 +15,9 @@ public class SendValidationResultActivity(ServiceBusClient serviceBusClient, ILo
         var body = JsonSerializer.Serialize(result);
         await using var sender = serviceBusClient.CreateSender(GlobalConstants.OutgoingQueueName);
         await sender.SendMessageAsync(new ServiceBusMessage(body), context.CancellationToken);
-        logger.LogInformation("Sent validation response");
+        LogMessageSent(result.WaitingInstanceId);
     }
+
+    [LoggerMessage(LogLevel.Information, "Sent validation response for waiting instance {WaitingInstanceId}")]
+    partial void LogMessageSent(string waitingInstanceId);
 }
